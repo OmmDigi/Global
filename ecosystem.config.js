53a8c9fd4d9f82f8efaa6a98fabf55fdddbdebf1
OmmDigi <ommdigitalsolution@gmail.com>
2025-09-02 13:29:23 +0530

    update the ecosystem.confg

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,13 +16,25 @@ module.exports = {
       name: "gti-api",
       script: "dist/index.js",
       cwd: "/var/www/globaltechnicalinstitute/api",
-      env: { PORT: 4001, HOST: "127.0.0.1", NODE_ENV: "production" }
+      env: { PORT: 4001, HOST: "127.0.0.1", NODE_ENV: "production" },
     },
     {
       name: "gti-upload",
       script: "dist/server.js",
       cwd: "/var/www/globaltechnicalinstitute/upload",
-      env: { PORT: 5001, HOST: "127.0.0.1", NODE_ENV: "production" }
-    }
-  ]
-}
+      env: { PORT: 5001, HOST: "127.0.0.1", NODE_ENV: "production" },
+    },
+    {
+      name: "gti-essl-server",
+      script: "http-server.js",
+      cwd: "/var/www/globaltechnicalinstitute/essl",
+      env: { TCP_NET_PORT: 6001, HOST: "127.0.0.1", NODE_ENV: "production" },
+    },
+    {
+      name: "gti-essl-net-server",
+      script: "tcp-server.js",
+      cwd: "/var/www/globaltechnicalinstitute/essl",
+      env: { TCP_NET_PORT: 7001, HOST: "127.0.0.1", NODE_ENV: "production" },
+    },
+  ],
+};
