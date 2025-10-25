a45fa03310701e459ef8a9ba9ab6ccce89e093bb
OmmDigi <ommdigitalsolution@gmail.com>
2025-10-25 12:07:22 +0530

    tag manager add

--- Admin/free-react-tailwind-admin-dashboard-main/src/pages/admin/AdmissionAdmin/AdmissionAdmin.tsx
+++ Admin/free-react-tailwind-admin-dashboard-main/src/pages/admin/AdmissionAdmin/AdmissionAdmin.tsx
@@ -656,6 +656,18 @@ export default function AdmissionAdmin() {
                                   ))}
                               </select>
                             </div>
+                            <div className="mt-0">
+                              <label className="block text-sm text-start text-gray-700 mb-1">
+                                Date <span className="text-red-500">*</span>
+                              </label>
+                              <input
+                                type="date"
+                                name="date"
+                                value={formData.date}
+                                onChange={handleInputChange}
+                                className="w-48 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                              />
+                            </div>
                           </div>
 
                           {/* Personal Details */}
