beac40f8514a04dde9f04e1f9b8426144f8e4c10
OmmDigi <ommdigitalsolution@gmail.com>
2025-12-03 13:54:22 +0530

    fix

--- api/src/controllers/admission.controller.ts
+++ api/src/controllers/admission.controller.ts
@@ -796,7 +796,7 @@ export const changeStudentAdmisionCourse = asyncErrorHandler(
     await doTransition(async (client) => {
       await client.query(
         `UPDATE enrolled_courses SET course_id = $1, batch_id = $2, session_id = $3 WHERE form_id IN (${placeholder})`,
-        [value.course_id, value.batch_id, value.session_id, uniqueFormIds]
+        [value.course_id, value.batch_id, value.session_id, ...uniqueFormIds]
       );
 
       await client.query(
