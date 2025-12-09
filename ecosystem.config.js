2952d6eaff6c9d6791248707096d754b41a41579
OmmDigi <ommdigitalsolution@gmail.com>
2025-12-09 10:41:05 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "gti-frontend",
       script: "./node_modules/.bin/next",
       args: ["start", "-p", "3001", "-H", "127.0.0.1"],
-      cwd: "/var/www/globaltechnicalinstitute/client",
+      cwd: "/var/www/Global/client",
       env: {
         NODE_ENV: "production",
         PORT: "3001",
@@ -14,26 +14,26 @@ module.exports = {
     {
       name: "gti-api",
       script: "dist/index.js",
-      cwd: "/var/www/globaltechnicalinstitute/api",
+      cwd: "/var/www/Global/api",
       env: { PORT: 4001, HOST: "127.0.0.1", NODE_ENV: "production" },
     },
     {
       name: "gti-upload",
       script: "dist/server.js",
-      cwd: "/var/www/globaltechnicalinstitute/upload",
+      cwd: "/var/www/Global/upload",
       env: { PORT: 5001, HOST: "127.0.0.1", NODE_ENV: "production" },
     },
     {
       name: "gti-essl-server",
       script: "http-server.js",
-      cwd: "/var/www/globaltechnicalinstitute/essl",
+      cwd: "/var/www/Global/essl",
       env: { TCP_NET_PORT: 6001, HOST: "127.0.0.1", NODE_ENV: "production" },
     },
-    {
-      name: "gti-essl-net-server",
-      script: "tcp-server.js",
-      cwd: "/var/www/globaltechnicalinstitute/essl",
-      env: { TCP_NET_PORT: 7001, HOST: "127.0.0.1", NODE_ENV: "production" },
-    },
+    // {
+    //   name: "gti-essl-net-server",
+    //   script: "tcp-server.js",
+    //   cwd: "/var/www/Global/essl",
+    //   env: { TCP_NET_PORT: 7001, HOST: "127.0.0.1", NODE_ENV: "production" },
+    // },
   ],
 };
